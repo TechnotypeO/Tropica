bb50f7be096727e67a061a26bb86416814d3203d
TechnotypeO <gamingmasterdonttryit78@gmail.com>
2020-02-11 17:18:58 +0100

    Initial commit

---
+++ src/main/java/me/tecc/tropica/tropica/Tropica.java
@@ -0,0 +1,17 @@
+package me.tecc.tropica.tropica;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Tropica extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
