eb1e50c92da98615ab3cff3888b3e0db2685ee90
funtodead <totominc@gmail.com>
2020-02-11 23:54:08 +0200

    Moveed & help

--- src/main/java/me/tecc/tropica/Tropica.java
+++ src/main/java/me/tecc/tropica/Tropica.java
@@ -1,6 +1,7 @@
 package me.tecc.tropica;
 
 import com.google.gson.Gson;
+import me.tecc.tropica.commands.HelpCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Tropica extends JavaPlugin {
@@ -8,10 +9,8 @@ public final class Tropica extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-    }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+       getCommand("help").setExecutor(new HelpCommand());
+
     }
 }
---
+++ src/main/java/me/tecc/tropica/commands/HelpCommand.java
@@ -0,0 +1,93 @@
+package me.tecc.tropica.commands;
+
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HelpCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+
+        if (sender instanceof Player) {//checks if the sender is a player
+            //there is your player variable
+            Player player = (Player) sender;
+            if (args.length == 0) {
+                player.sendMessage(toColor("&FUnknown command, try using /help"));
+                return true;
+            }
+
+            if (args.length >= 1) {
+                if (!args[0].equalsIgnoreCase("help")) {
+                    player.sendMessage(toColor("&FUnknown command, try using  /help"));
+                    return true;
+                }
+            }
+
+
+
+            //checking if player var
+            if (player.hasPermission("gm.admin"))
+                if (player.hasPermission("gm.help")) {
+                    //do stuff if he doe
+                    player.sendMessage(toColor("&2&LTropica &7» &7Help Menu"));
+                    player.sendMessage("");
+
+
+                    /*Ignore the GM stuff, we will change it soon, ~ funtodead  //Also If you see this Please remove the permission requirement from this command. and also the else
+
+                    /*1 */net.md_5.bungee.api.chat.TextComponent one = new TextComponent(toColor("&a1&f. /gm help &f- &7Sends this message."));
+                    one.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/gm help)")).create()));
+                    player.spigot().sendMessage(one);
+
+                    net.md_5.bungee.api.chat.TextComponent two = new TextComponent(toColor("&a2&f. /gmc [player] &f- &7Changes gamemode to Creative. (&fHover to type)"));
+                    two.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/gmc)")).create()));
+                    player.spigot().sendMessage(two);
+
+                    net.md_5.bungee.api.chat.TextComponent three = new TextComponent(toColor("&a3&f. /gms [player] &f- &7Changes gamemode to Survival. (&fHover to type)"));
+                    three.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/gms)")).create()));
+                    player.spigot().sendMessage(three);
+
+                    net.md_5.bungee.api.chat.TextComponent four = new TextComponent(toColor("&a4&f. /gms [player] &f- &7Changes gamemode to Adventure. (&fHover to type)"));
+                    four.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/gma)")).create()));
+                    player.spigot().sendMessage(four);
+
+                    net.md_5.bungee.api.chat.TextComponent five = new TextComponent(toColor("&a5&f. /gms [player] &f- &7Changes gamemode to Spectator. (&fHover to type)"));
+                    five.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/gmspec)")).create()));
+                    player.spigot().sendMessage(five);
+                      //For next Page
+                    net.md_5.bungee.api.chat.TextComponent six = new TextComponent(toColor("&eNext Page ->"));
+                    six.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&7Click &ehere &7To go to next page")).create()));
+                    six.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "help 2" ));
+                    player.spigot().sendMessage(six);
+
+                       //Last updated by funtodead at 12/2/2020
+
+
+
+
+
+                    /* No perms for the player*/
+                    //else if he doesn't have
+                } else {
+                    //sends him error
+                    player.sendMessage(toColor("&cNo Permission."));
+                }
+            /* For console */
+        } else {
+            sender.sendMessage(toColor("&cYou must be a player to execute this command"));
+        }
+
+        return true;
+    }
+
+    private String toColor(String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
+    }
+
+}
