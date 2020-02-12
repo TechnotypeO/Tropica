2d7a99674564f7e1eaf8b9d8a8aac4fdb98da325
funtodead <totominc@gmail.com>
2020-02-12 23:15:12 +0200

    Commit

--- src/main/java/me/tecc/tropica/commands/HelpCommand.java
+++ src/main/java/me/tecc/tropica/commands/HelpCommand.java
@@ -24,27 +24,31 @@ public class HelpCommand implements CommandExecutor {
             /*Ignore the GM stuff, we will change it soon, ~ funtodead  //Also If you see this Please remove the permission requirement from this command. and also the else
 
             /*1 */
-            net.md_5.bungee.api.chat.TextComponent one = new TextComponent(toColor("&a1&f. /gm help &f- &7Sends this message."));
-            one.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/gm help)")).create()));
+            net.md_5.bungee.api.chat.TextComponent one = new TextComponent(toColor("&e1&f. /Claim &f- &7Claim a land. (&fHover to type)."));
+            one.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/Claim)")).create()));
+            one.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "claim"));
             player.spigot().sendMessage(one);
 
-            net.md_5.bungee.api.chat.TextComponent two = new TextComponent(toColor("&a2&f. /gmc [player] &f- &7Changes gamemode to Creative. (&fHover to type)"));
+            net.md_5.bungee.api.chat.TextComponent two = new TextComponent(toColor("&e2&f. /unclaim &f- &7unclaim the land you're next to (&fHover to type)"));
             two.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/gmc)")).create()));
+            two.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "unclaim"));
             player.spigot().sendMessage(two);
 
-            net.md_5.bungee.api.chat.TextComponent three = new TextComponent(toColor("&a3&f. /gms [player] &f- &7Changes gamemode to Survival. (&fHover to type)"));
+            net.md_5.bungee.api.chat.TextComponent three = new TextComponent(toColor("&e3&f. /abandonallclaims &f- &7Unclaim all Land you ever claimed. (&fHover to type)"));
             three.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/gms)")).create()));
+            three.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "abandonallclaims"));
             player.spigot().sendMessage(three);
 
-            net.md_5.bungee.api.chat.TextComponent four = new TextComponent(toColor("&a4&f. /gms [player] &f- &7Changes gamemode to Adventure. (&fHover to type)"));
+
+            net.md_5.bungee.api.chat.TextComponent four = new TextComponent(toColor("&e4&f. /gms [player] &f- &7Changes gamemode to Adventure. (&fHover to type)"));
             four.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/gma)")).create()));
             player.spigot().sendMessage(four);
 
-            net.md_5.bungee.api.chat.TextComponent five = new TextComponent(toColor("&a5&f. /gms [player] &f- &7Changes gamemode to Spectator. (&fHover to type)"));
+            net.md_5.bungee.api.chat.TextComponent five = new TextComponent(toColor("&e5&f. /gms [player] &f- &7Changes gamemode to Spectator. (&fHover to type)"));
             five.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/gmspec)")).create()));
             player.spigot().sendMessage(five);
             //For next Page
-            net.md_5.bungee.api.chat.TextComponent six = new TextComponent(toColor("&eNext Page ->"));
+            net.md_5.bungee.api.chat.TextComponent six = new TextComponent(toColor("&6Next Page ->"));
             six.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&7Click &ehere &7To go to next page")).create()));
             six.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "help 2"));
             player.spigot().sendMessage(six);
