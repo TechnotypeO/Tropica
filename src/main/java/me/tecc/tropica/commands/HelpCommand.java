3778579604e339d49e583d364f1e12827af44e3d
funtodead <totominc@gmail.com>
2020-02-12 23:18:17 +0200

    Commit

--- src/main/java/me/tecc/tropica/commands/HelpCommand.java
+++ src/main/java/me/tecc/tropica/commands/HelpCommand.java
@@ -26,31 +26,30 @@ public class HelpCommand implements CommandExecutor {
             /*1 */
             net.md_5.bungee.api.chat.TextComponent one = new TextComponent(toColor("&e1&f. /Claim &f- &7Claim a land. (&fHover to type)."));
             one.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/Claim)")).create()));
-            one.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "claim"));
+            one.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/claim"));
             player.spigot().sendMessage(one);
-
-            net.md_5.bungee.api.chat.TextComponent two = new TextComponent(toColor("&e2&f. /unclaim &f- &7unclaim the land you're next to (&fHover to type)"));
+            player.sendMessage("");
+            net.md_5.bungee.api.chat.TextComponent two = new TextComponent(toColor("&e2&f. /unclaim &f- &7unclaim the land you're next to &f(Hover to type)"));
             two.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/gmc)")).create()));
-            two.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "unclaim"));
+            two.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/unclaim"));
             player.spigot().sendMessage(two);
-
-            net.md_5.bungee.api.chat.TextComponent three = new TextComponent(toColor("&e3&f. /abandonallclaims &f- &7Unclaim all Land you ever claimed. (&fHover to type)"));
+            player.sendMessage("");
+            net.md_5.bungee.api.chat.TextComponent three = new TextComponent(toColor("&e3&f. /abandonallclaims &f- &7Unclaim all Land you ever claimed. &f(Hover to type)"));
             three.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/gms)")).create()));
-            three.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "abandonallclaims"));
+            three.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/abandonallclaims"));
             player.spigot().sendMessage(three);
-
-
+            player.sendMessage("");
             net.md_5.bungee.api.chat.TextComponent four = new TextComponent(toColor("&e4&f. /gms [player] &f- &7Changes gamemode to Adventure. (&fHover to type)"));
             four.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/gma)")).create()));
             player.spigot().sendMessage(four);
-
+            player.sendMessage("");
             net.md_5.bungee.api.chat.TextComponent five = new TextComponent(toColor("&e5&f. /gms [player] &f- &7Changes gamemode to Spectator. (&fHover to type)"));
             five.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&b&lClick to type in chat &7(/gmspec)")).create()));
             player.spigot().sendMessage(five);
             //For next Page
             net.md_5.bungee.api.chat.TextComponent six = new TextComponent(toColor("&6Next Page ->"));
             six.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(toColor("&7Click &ehere &7To go to next page")).create()));
-            six.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "help 2"));
+            six.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/help 2"));
             player.spigot().sendMessage(six);
         }
         return true;
