b1c34d372ca4613f06d0d890feda6fbac63d1c0f
TechnotypeO <gamingmasterdonttryit78@gmail.com>
2020-02-20 12:35:48 +0100

    Added saveAsync method to IStorage (See IStorage.saveAsync, line 13)
    I also included an implementation for AbstractContainer, see AbstractContainer.saveAsync (Line 56)

--- src/main/java/me/tecc/tropica/storage/IStorage.java
+++ src/main/java/me/tecc/tropica/storage/IStorage.java
@@ -9,4 +9,11 @@ public interface IStorage {
      * @return Whether or not the current storage could be saved.
      */
     boolean save();
+
+    /**
+     * Save the current storage asynchronously.
+     *
+     * @return Whether or not the current storage could be saved.
+     */
+    boolean saveAsync();
 }
