ba3a466071ec84047d83b5e61c8b8976fdcbefff
TechnotypeO <gamingmasterdonttryit78@gmail.com>
2020-02-20 12:54:02 +0100

    Update setupLibraries script & moved Spigot server library jar
    The Spigot server library jar (server-1.15.2-R0.1-SNAPSHOT.jar) was moved to the root libs directory.
    It was also renamed to spigot-server-1.15.2-R0.1-SNAPSHOT.jar.

--- setupLibraries.bat
+++ setupLibraries.bat
@@ -3,5 +3,6 @@
 echo Setting up required Maven libraries
 echo NOTE: This script assumes you have Maven installed.
 echo Maven 2.5 and later will work with this.
-mvn install:install-file -Dfile=./libs/org/spigotmc/server/1.15.2-R0.1-SNAPSHOT/server-1.15.2-R0.1-SNAPSHOT.jar -DgroupId=org.spigotmc.server -DartifactId=spigot-server -Dversion=1.15.2-R0.1-SNAPSHOT -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=./libs/spigot-server-1.15.2-R0.1-SNAPSHOT.jar -DgroupId=org.spigotmc.server -DartifactId=spigot-server -Dversion=1.15.2-R0.1-SNAPSHOT -Dpackaging=jar -DgeneratePom=true
 pause
+echo Libraries have now been set up. Happy programming :)
