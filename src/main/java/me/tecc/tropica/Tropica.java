4b915edc2b87c9871ac7545ea4f92523c0ac8f2c
MarcusSlover <mariuszek016@gmail.com>
2020-02-27 17:40:32 +0100

    + Added join reload event

--- src/main/java/me/tecc/tropica/Tropica.java
+++ src/main/java/me/tecc/tropica/Tropica.java
@@ -10,7 +10,10 @@ import me.tecc.tropica.sidebar.Rank;
 import me.tecc.tropica.storage.CollectionContainer;
 import me.tecc.tropica.storage.PlayerContainer;
 import me.tecc.tropica.storage.PublicContainer;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Tropica extends JavaPlugin {
@@ -39,6 +42,11 @@ public final class Tropica extends JavaPlugin {
        //Commands <-
         getCommand("broadcast").setExecutor(new BroadcastCommand());
         getCommand("help").setExecutor(new HelpCommand());
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            PlayerJoinEvent playerJoinEvent = new PlayerJoinEvent(player, "");
+            Bukkit.getPluginManager().callEvent(playerJoinEvent);
+        }
 
     }
 
