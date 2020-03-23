95dc9d7d6e4fee9116460fd4957b3d6f4f9c84c4
funtodead <totominc@gmail.com>
2020-03-23 15:04:53 +0200

    Added a player.sendmessage if enderchest Opened.                                                                     Please someone check the class and help me with a problem                                                                     i wrote there, also pm me after you read it (EnderChestCommand.java)

--- src/main/java/me/tecc/tropica/commands/EnderChestCommand.java
+++ src/main/java/me/tecc/tropica/commands/EnderChestCommand.java
@@ -1,18 +1,28 @@
 package me.tecc.tropica.commands;
 
+import me.tecc.tropica.TUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class EnderChestCommand implements CommandExecutor {
+    private Object Player;
+
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (commandSender instanceof Player) {
-            Player player = (Player) commandSender;
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
             player.openInventory(player.getEnderChest());
+            player.sendMessage(TUtil.toColor("&5&LENDERCHEST! &7Opened your &5Enderchest"));
+          //~funtodead - I couldn't find out how to add the right way to if player.closeinventory, to send him a message
+            //You closed your enderchest. if someone sees this. and can help. possibly pm me so i can also learn.
+
+
         }
+
 
         return true;
     }
 }
+
