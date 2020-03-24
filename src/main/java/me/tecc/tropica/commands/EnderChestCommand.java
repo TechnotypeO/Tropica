81c057060f61560cb91bd99d9971aa083a38e9ca
funtodead <totominc@gmail.com>
2020-03-24 22:35:34 +0200

    Added Messages for /ec

--- src/main/java/me/tecc/tropica/commands/EnderChestCommand.java
+++ src/main/java/me/tecc/tropica/commands/EnderChestCommand.java
@@ -1,13 +1,23 @@
 package me.tecc.tropica.commands;
 
 import me.tecc.tropica.TUtil;
+import me.tecc.tropica.Tropica;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
 
-public class EnderChestCommand implements CommandExecutor {
-    private Object Player;
+public class EnderChestCommand implements CommandExecutor, Listener {
+
+    public EnderChestCommand() {
+        Bukkit.getPluginManager().registerEvents(this, Tropica.getTropica());
+    }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -15,14 +25,18 @@ public class EnderChestCommand implements CommandExecutor {
             Player player = (Player) sender;
             player.openInventory(player.getEnderChest());
             player.sendMessage(TUtil.toColor("&5&LENDERCHEST! &7Opened your &5Enderchest"));
-          //~funtodead - I couldn't find out how to add the right way to if player.closeinventory, to send him a message
-            //You closed your enderchest. if someone sees this. and can help. possibly pm me so i can also learn.
-
-
         }
-
 
         return true;
     }
+
+    @EventHandler
+    public void EnderchestClose(InventoryCloseEvent e) {
+        if (e.getInventory().getType() == InventoryType.ENDER_CHEST) {
+            e.getPlayer().sendMessage(TUtil.toColor("&5&LENDERCHEST! &7Closed your &5Enderchest"));
+        }
+
+    }
+
 }
 
